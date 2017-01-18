1835161ab5de32651f1cd394b9476b053d555eb0
Michael Kantor <michael@layer.com>
2017-01-17 18:52:12 -0800

    Roll back change

--- src/client-authenticator.js
+++ src/client-authenticator.js
@@ -541,7 +541,7 @@ class ClientAuthenticator extends Root {
       }
       this.persistenceFeatures = {
         conversations: this.isPersistenceEnabled,
-        // channels: this.isPersistenceEnabled,
+        channels: this.isPersistenceEnabled,
         messages: this.isPersistenceEnabled,
         identities: this.isPersistenceEnabled,
         syncQueue: this.isPersistenceEnabled,
